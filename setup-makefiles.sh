e11eeb010aed3c5e7e53675b50cb8452fa1f21d4
CallMESuper <zj7859256@gmail.com>
2016-01-18 20:00:44 +0800

    mv com.qualcomm.location.apk to app

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -92,7 +92,7 @@ ifeq (\$(TARGET_DEVICE),$DEVICE)
 include \$(CLEAR_VARS)
 LOCAL_MODULE := com.qualcomm.location
 LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
+LOCAL_SRC_FILES := proprietary/app/com.qualcomm.location/com.qualcomm.location.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
