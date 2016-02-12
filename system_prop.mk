89be17149e4de786a2c9ad3ea96a2cf643b42230
CallMESuper <zj7859256@gmail.com>
2016-02-12 13:59:54 +0800

    update from cmremix

--- system_prop.mk
+++ system_prop.mk
@@ -40,7 +40,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.qmi.adb_logmask=0 \
     persist.radio.add_power_save=1 \
     persist.radio.lte_vrat_report=1 \
-    ro.telephony.mms_data_profile=5
+    ro.telephony.mms_data_profile=5 \
+    ro.telephony.ril.config=setPrefNwTypeOnUnsolConnected
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
